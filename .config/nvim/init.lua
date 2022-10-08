bd0e0a863afd6b85218afc6e7248b1f5c82fff08
jdefez <jdefez@gmail.com>
2022-10-08 13:09:45 +0200

    fix indentation settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,3 +11,17 @@ vim.cmd('colorscheme catppuccin')
 -- undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.cmd [[
+  " Specific syntax settings
+
+  au BufRead,BufNewFile *.blade.php set ft=html
+  autocmd Filetype python setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
+  autocmd Filetype apache setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
+  autocmd Filetype php setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
+
+  " avoiding phpactor to create variables with 2 $ sign
+
+  autocmd FileType php set iskeyword+=$
+  autocmd FileType php setlocal omnifunc=phpactor#Complete
+]]
