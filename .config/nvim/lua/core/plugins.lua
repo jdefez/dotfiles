4f5959aaad7411670085a3f236330e3f3b2afa5d
jdefez <jdefez@gmail.com>
2022-12-17 12:57:55 +0100

    updating config with missing plugins

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -1,8 +1,8 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -11,69 +11,72 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+  use "wbthomason/packer.nvim"
   -- themes
 
-  use 'projekt0n/github-nvim-theme'
+  use "projekt0n/github-nvim-theme"
 
   -- navigation
 
-  use 'TheLeoP/fern-renderer-web-devicons.nvim'
-  use 'feline-nvim/feline.nvim'
-  use 'karb94/neoscroll.nvim'
-  use 'rmagatti/auto-session'
-  use 'lambdalisue/fern.vim'
-  use 'nyngwang/murmur.lua'
-  use 'aserowy/tmux.nvim'
-  use 'preservim/vimux'
+  use "TheLeoP/fern-renderer-web-devicons.nvim"
+  use "beauwilliams/focus.nvim"
+  use "feline-nvim/feline.nvim"
+  use "karb94/neoscroll.nvim"
+  use "rmagatti/auto-session"
+  use "lambdalisue/fern.vim"
+  use "nyngwang/murmur.lua"
+  use { "phaazon/hop.nvim", branch = "v2" }
+  use "aserowy/tmux.nvim"
+  use "preservim/vimux"
   use {
-    'akinsho/bufferline.nvim',
-    tag = '*',
-    requires = 'kyazdani42/nvim-web-devicons',
+    "akinsho/bufferline.nvim",
+    tag = "*",
+    requires = "kyazdani42/nvim-web-devicons",
   }
   use {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     requires = {
-      'nvim-lua/plenary.nvim',
-      -- 'nvim-telescope/telescope-dap.nvim'
+      "nvim-lua/plenary.nvim",
+      -- "nvim-telescope/telescope-dap.nvim"
     }
   }
-  use 'kazhala/close-buffers.nvim'
-  use 'xiyaowong/virtcolumn.nvim'
+  use "kazhala/close-buffers.nvim"
+  use "xiyaowong/virtcolumn.nvim"
   -- git
   use {
-    'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
+    "lewis6991/gitsigns.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
   }
-  use 'tpope/vim-fugitive'
+  use "tpope/vim-fugitive"
 
   -- editing
 
-  use 'anuvyklack/pretty-fold.nvim'
-  use { 'kevinhwang91/nvim-bqf', ft = 'qf' }
-  use 'pocco81/auto-save.nvim'
-  use 'windwp/nvim-autopairs'
-  use { 'mg979/vim-visual-multi', { branch = 'master' } }
-  use 'b3nj5m1n/kommentary'
-  use 'phpactor/phpactor'
-  use 'vim-test/vim-test'
-  use { 'ms-jpq/coq_nvim', branch = 'coq' }
-  use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
+  use "anuvyklack/pretty-fold.nvim"
+  use { "kevinhwang91/nvim-bqf", ft = "qf" }
+  use "pocco81/auto-save.nvim"
+  use "windwp/nvim-autopairs"
+  use { "mg979/vim-visual-multi", { branch = "master" } }
+  use "b3nj5m1n/kommentary"
+  use "phpactor/phpactor"
+  use "vim-test/vim-test"
+  use { "ms-jpq/coq_nvim", branch = "coq" }
+  use { "ms-jpq/coq.artifacts", branch = "artifacts" }
 
   -- lsp
 
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'jose-elias-alvarez/null-ls.nvim'
+  use "nvim-treesitter/nvim-treesitter"
+  use "jose-elias-alvarez/null-ls.nvim"
   use {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig'
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig"
   }
+  use { "glepnir/lspsaga.nvim", branch = "main" }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
---
+++ .config/nvim/lua/core/plugins_config/lsp-saga.lua
@@ -0,0 +1,65 @@
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+  }
+)
+
+local keymap = vim.keymap.set
+local saga = require("lspsaga")
+
+saga.init_lsp_saga({
+  diagnostic_header = { "😡", "😥", "😤", "😐" },
+})
+
+-- Lsp finder find the symbol definition implement reference
+-- if there is no implement it will hide
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+keymap("n", "lh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+
+-- Code action
+keymap({ "n", "v" }, "<leader>lx", "<cmd>Lspsaga code_action<CR>", { silent = true })
+
+-- Rename
+keymap("n", "lr", "<cmd>Lspsaga rename<CR>", { silent = true })
+
+-- Peek Definition
+-- you can edit the definition file in this flaotwindow
+keymap("n", "<leader>pd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+
+-- Show line diagnostics
+keymap("n", "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+
+-- Show cursor diagnostics
+keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+
+-- Diagnostic jump can use `<c-o>` to jump back
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+
+-- Only jump to error
+keymap("n", "[E", function()
+  require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+keymap("n", "]E", function()
+  require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+
+-- Outline
+keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
+
+-- Hover Doc
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+
+-- Float terminal
+-- keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
+
+-- if you want to pass some cli command into a terminal you can do it like this
+-- open lazygit in lspsaga float terminal
+-- keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm lazygit<CR>", { silent = true })
+
+-- close floaterm
+-- keymap("t", "<A-d>", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
