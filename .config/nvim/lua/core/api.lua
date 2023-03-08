53d4810770191197a153f11b9f725e804c1136cb
jean-francois Defez <jean-francois@ayctor.com>
2023-03-08 09:17:03 +0100

    updating kanagawa settings + enable focus

--- .config/nvim/lua/core/api.lua
+++ .config/nvim/lua/core/api.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = ' '
-vim.g.termguicolors = true
 vim.wo.colorcolumn = '81'
 vim.wo.relativenumber = true
 vim.wo.number = true
@@ -23,7 +22,7 @@ vim.opt.wildmode = 'longest:full,full'
 vim.opt.title = true
 vim.opt.mouse = 'a'
 vim.opt.list = true
-vim.opt.listchars = {tab = '~ ', trail = '.'}
+vim.opt.listchars = { tab = '~ ', trail = '.' }
 vim.opt.backup = true
 vim.opt.backupdir:remove('.') -- no backup file in the current dir
 vim.opt.clipboard = 'unnamedplus' -- use system clipboard
@@ -36,11 +35,5 @@ vim.cmd [[
   autocmd Filetype python setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
   autocmd Filetype apache setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
   autocmd Filetype php setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
-
-  " avoiding phpactor to create variables with 2 $ sign
-  "autocmd FileType phpset iskeyword+=$
-  "autocmd FileType php setlocal omnifunc=phpactor#Complete
-
-  " abbreviations
-  "autocmd FileType php iabbrev <buffer> t@ /** @test */<CR>
+  autocmd Filetype lua setlocal expandtab tabstop=2 softtabstop=2 shiftwidth=2
 ]]
--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -1,108 +1,108 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+        local fn = vim.fn
+        local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+        if fn.empty(fn.glob(install_path)) > 0 then
+                fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+                vim.cmd [[packadd packer.nvim]]
+                return true
+        end
+        return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-  use "wbthomason/packer.nvim"
+        use "wbthomason/packer.nvim"
 
-  -- utility
+        -- utility
 
-  use "TheLeoP/fern-renderer-web-devicons.nvim"
-  use "nvim-tree/nvim-web-devicons"
-  use "jghauser/mkdir.nvim"
+        use "TheLeoP/fern-renderer-web-devicons.nvim"
+        use "nvim-tree/nvim-web-devicons"
+        use "jghauser/mkdir.nvim"
 
-  -- themes
-  use "lourenci/github-colors"
-  use "sainnhe/edge"
-  use {
-    "mcchrish/zenbones.nvim",
-    requires = "rktjmp/lush.nvim"
-  }
-  use "ishan9299/modus-theme-vim"
-  use "rebelot/kanagawa.nvim"
-  use "loctvl842/monokai-pro.nvim"
+        -- themes
+        use "lourenci/github-colors"
+        use "sainnhe/edge"
+        use {
+                "mcchrish/zenbones.nvim",
+                requires = "rktjmp/lush.nvim"
+        }
+        use "ishan9299/modus-theme-vim"
+        use "rebelot/kanagawa.nvim"
+        use "loctvl842/monokai-pro.nvim"
 
-  -- navigation
+        -- navigation
 
-  use "freddiehaddad/feline.nvim"
-  use "karb94/neoscroll.nvim"
-  use "olimorris/persisted.nvim"
-  use "lambdalisue/fern.vim"
-  use "nyngwang/murmur.lua"
-  use "aserowy/tmux.nvim"
-  use "preservim/vimux"
-  use {
-    "akinsho/bufferline.nvim",
-    tag = "v3.*",
-    requires = "nvim-tree/nvim-web-devicons",
-  }
-  use {
-    "nvim-telescope/telescope.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-    }
-  }
-  use "kazhala/close-buffers.nvim"
-  use "xiyaowong/virtcolumn.nvim"
+        use "freddiehaddad/feline.nvim"
+        use "karb94/neoscroll.nvim"
+        use "olimorris/persisted.nvim"
+        use "lambdalisue/fern.vim"
+        use "nyngwang/murmur.lua"
+        use "aserowy/tmux.nvim"
+        use "preservim/vimux"
+        use {
+                "akinsho/bufferline.nvim",
+                tag = "v3.*",
+                requires = "nvim-tree/nvim-web-devicons",
+        }
+        use {
+                "nvim-telescope/telescope.nvim",
+                requires = {
+                        "nvim-lua/plenary.nvim",
+                }
+        }
+        use "kazhala/close-buffers.nvim"
+        use "xiyaowong/virt-column.nvim"
 
-  -- git
+        -- git
 
-  use {
-    "lewis6991/gitsigns.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-  }
-  use "tpope/vim-fugitive"
+        use {
+                "lewis6991/gitsigns.nvim",
+                requires = { "nvim-lua/plenary.nvim" },
+        }
+        use "tpope/vim-fugitive"
 
-  -- language
-  use "jparise/vim-graphql"
+        -- language
+        use "jparise/vim-graphql"
 
-  -- editing
+        -- editing
 
-  use { "ms-jpq/coq_nvim", branch = "coq" }
-  use { "ms-jpq/coq.artifacts", branch = "artifacts" }
+        use { "ms-jpq/coq_nvim", branch = "coq" }
+        use { "ms-jpq/coq.artifacts", branch = "artifacts" }
 
-  use "Pocco81/true-zen.nvim"
-  -- use "beauwilliams/focus.nvim" -- crashes session manager
-  use "anuvyklack/pretty-fold.nvim"
-  use { "mg979/vim-visual-multi", { branch = "master" } }
-  use { "kevinhwang91/nvim-bqf", ft = "qf" }
-  use "pocco81/auto-save.nvim"
-  use "windwp/nvim-autopairs"
-  use "numToStr/FTerm.nvim"
-  use "b3nj5m1n/kommentary"
-  use { "phaazon/hop.nvim", branch = "v2" }
-  use "phpactor/phpactor"
-  use "vim-test/vim-test"
+        use "Pocco81/true-zen.nvim"
+        use "beauwilliams/focus.nvim"
+        use "anuvyklack/pretty-fold.nvim"
+        use { "mg979/vim-visual-multi", { branch = "master" } }
+        use { "kevinhwang91/nvim-bqf", ft = "qf" }
+        use "pocco81/auto-save.nvim"
+        use "windwp/nvim-autopairs"
+        use "numToStr/FTerm.nvim"
+        use "b3nj5m1n/kommentary"
+        use { "phaazon/hop.nvim", branch = "v2" }
+        use "phpactor/phpactor"
+        use "vim-test/vim-test"
 
-  -- lsp
+        -- lsp
 
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-  use "jose-elias-alvarez/null-ls.nvim"
-  use {
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    requires = {
-      { "nvim-tree/nvim-web-devicons" }
-    },
-  }
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig"
-  }
+        use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+        use "jose-elias-alvarez/null-ls.nvim"
+        use {
+                "glepnir/lspsaga.nvim",
+                branch = "main",
+                requires = {
+                        { "nvim-tree/nvim-web-devicons" }
+                },
+        }
+        use {
+                "williamboman/mason.nvim",
+                "williamboman/mason-lspconfig.nvim",
+                "neovim/nvim-lspconfig"
+        }
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+        -- Automatically set up your configuration after cloning packer.nvim
+        -- Put this at the end after all plugins
+        if packer_bootstrap then
+                require("packer").sync()
+        end
 end)
