ce98e37b22c174ed160315168d423b40828bd82b
jdefez <jdefez@gmail.com>
2023-03-19 14:08:42 +0100

    everforest setup update

--- .config/nvim/lua/core/plugins_config/everforest.lua
+++ .config/nvim/lua/core/plugins_config/everforest.lua
@@ -1,6 +1,6 @@
 require("everforest").setup({
   -- Controls the "hardness" of the background: "soft", "medium" or "hard".
-  background = "soft",
+  background = "hard",
   -- How much of the background should be transparent. Options are 0, 1 or 2.
   transparent_background_level = 0,
   -- Whether italics should be used for keywords, builtin types and more.
