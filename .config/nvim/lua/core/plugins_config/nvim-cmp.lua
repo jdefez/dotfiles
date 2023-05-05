884bfda19b001f4b009f94dfea01e171aa9057e0
jean-francois Defez <jean-francois@ayctor.com>
2023-05-05 10:43:56 +0200

    fix(cmp): missing icon

--- .config/nvim/lua/core/plugins_config/nvim-cmp.lua
+++ .config/nvim/lua/core/plugins_config/nvim-cmp.lua
@@ -13,18 +13,30 @@ end
 
 local kind_icons = {
   Text = "",
-  Method = "",
-  Function = "",
+  -- Method = "󰆧",
+  -- Function = "󰊕",
   Constructor = "",
+  -- Field = "󰇽",
+  -- Variable = "󰂡",
+  -- Class = "󰠱",
   Interface = "",
   Module = "",
+  -- Property = "󰜢",
   Unit = "",
+  -- Value = "󰎠",
   Enum = "",
+  -- Keyword = "󰌋",
   Snippet = "",
+  -- Color = "󰏘",
+  -- File = "󰈙",
   Reference = "",
+  -- Folder = "󰉋",
   EnumMember = "",
+  -- Constant = "󰏿",
   Struct = "",
   Event = "",
+  -- Operator = "󰆕",
+  -- TypeParameter = "󰅲",
 }
 
 cmp.setup({
@@ -70,7 +82,7 @@ cmp.setup({
   formatting = {
     format = function(entry, vim_item)
       -- Kind icons
-      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.kind = string.format('%s %s', (kind_icons[vim_item.kind]  or ''), vim_item.kind) -- This concatonates the icons with the name of the item kind
       -- Source
       vim_item.menu = ({
         buffer = "[Buffer]",
