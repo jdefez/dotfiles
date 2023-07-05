c368073d964e2b4c841e4eaf3e0a03f0121dfbcb
jean-francois Defez <jean-francois@ayctor.com>
2023-07-05 13:22:54 +0200

    fix: fugitive mappings

--- .config/nvim/lua/core/plugins_config/fugitive.lua
+++ .config/nvim/lua/core/plugins_config/fugitive.lua
@@ -1,3 +1,3 @@
-vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit!<CR>')
-vim.keymap.set('n', 'dgh', ':diffget //2<CR>')
-vim.keymap.set('n', 'dgl', ':diffget //3<CR>')
+vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit!<CR>', { silent = true })
+vim.keymap.set('n', 'dgh', ':diffget //2<CR>', { silent = true })
+vim.keymap.set('n', 'dgl', ':diffget //3<CR>', { silent = true })
