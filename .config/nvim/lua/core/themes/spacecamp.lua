91b4655e0eb29abd03006aa75b1d3eeae39d48be
jean-francois Defez <jean-francois@ayctor.com>
2023-07-08 18:53:08 +0200

    fix: missing undercurl

--- .config/nvim/lua/core/themes/spacecamp.lua
+++ .config/nvim/lua/core/themes/spacecamp.lua
@@ -1,2 +1,7 @@
 vim.cmd("colorscheme spacecamp_lite")
 vim.cmd("set background=dark")
+
+vim.cmd([[
+  highlight DiagnosticUnderlineWarn gui=undercurl
+  highlight DiagnosticUnderlineError gui=undercurl
+]])
