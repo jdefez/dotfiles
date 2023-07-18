8b9a3a24fe997366354a08db437e375cf32d3437
jean-francois Defez <jean-francois@ayctor.com>
2023-07-18 14:10:12 +0200

    feat(solarized): adding undercurl

--- .config/nvim/lua/core/themes/solarized-light.lua
+++ .config/nvim/lua/core/themes/solarized-light.lua
@@ -17,13 +17,18 @@ require('solarized').setup({
     -- local blend = colorhelper.blend
 
     return {
-      ColorColumn = { bg = darken(colors.base02, 0.5) },
+      ColorColumn = { bg = darken(colors.base02, 0.5) }
     }
   end,
 });
 
 vim.cmd("colorscheme solarized")
 vim.cmd("set background=light")
+
+vim.cmd([[
+  highlight DiagnosticUnderlineWarn gui=undercurl
+  highlight DiagnosticUnderlineError gui=undercurl
+]])
 
 
 -- vim.cmd [[
