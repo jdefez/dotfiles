1f4f32625e3547c5f3ae55e327fe253bf394798d
jdefez <jdefez@gmail.com>
2023-10-01 12:57:44 +0200

    scheme(palette): setup neogit highlight groups

--- .config/nvim/lua/core/themes/palette-dark.lua
+++ .config/nvim/lua/core/themes/palette-dark.lua
@@ -2,20 +2,22 @@ require("palette").setup({
   palettes = {
     -- dark or light
     main = "dark",
-
     -- pastel, bright or dark
-    accent = "pastel",
-    state = "pastel",
+    accent = "bright",
+    state = "bright",
   },
-
   italics = true,
   bold = true,
   transparent_background = false,
 })
 
-vim.cmd("colorscheme palette")
-
 vim.cmd([[
-  highlight DiagnosticUnderlineWarn gui=undercurl
+  colorscheme palette
   highlight DiagnosticUnderlineError gui=undercurl
+  highlight DiagnosticUnderlineWarn gui=undercurl
+  highlight NoiceCursor guibg=darkgrey
+  highlight NeogitDiffAdd guifg=#56A665 guibg=#e9e9ed
+  highlight NeogitDiffAddHighlight guifg=#56A665 guibg=#e9e9ed
+  highlight NeogitDiffDelete guifg=#B15555 guibg=#e9e9ed
+  highlight NeogitDiffDeleteHighlight guifg=#B15555 guibg=#e9e9ed
 ]])
--- .config/nvim/lua/core/themes/palette-light.lua
+++ .config/nvim/lua/core/themes/palette-light.lua
@@ -2,20 +2,22 @@ require("palette").setup({
   palettes = {
     -- dark or light
     main = "light",
-
     -- pastel, bright or dark
-    accent = "bright",
-    state = "bright",
+    accent = "dark",
+    state = "dark",
   },
-
   italics = true,
   bold = true,
   transparent_background = false,
 })
 
-vim.cmd("colorscheme palette")
-
 vim.cmd([[
-  highlight DiagnosticUnderlineWarn gui=undercurl
+  colorscheme palette
   highlight DiagnosticUnderlineError gui=undercurl
+  highlight DiagnosticUnderlineWarn gui=undercurl
+  highlight NoiceCursor guibg=darkgrey
+  highlight NeogitDiffAdd guifg=#56A665 guibg=#e9e9ed
+  highlight NeogitDiffAddHighlight guifg=#56A665 guibg=#e9e9ed
+  highlight NeogitDiffDelete guifg=#B15555 guibg=#e9e9ed
+  highlight NeogitDiffDeleteHighlight guifg=#B15555 guibg=#e9e9ed
 ]])
