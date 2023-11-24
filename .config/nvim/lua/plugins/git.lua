273f13513c8210920c0be1e446352a8491833af5
jean-francois Defez <jean-francois@ayctor.com>
2023-11-24 08:51:45 +0100

    plug: fix conflict mappings + adding lua copilot

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,26 +2,26 @@ return {
 	{
 		"akinsho/git-conflict.nvim",
 		version = "*",
-		config = {
-			default_mappings = true, -- disable buffer local mapping created by this plugin
-			default_commands = true, -- disable commands created by this plugin
-			disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
-			list_opener = "copen", -- command or function to open the conflicts list
-			highlights = { -- They must have background color, otherwise the default color will be used
-				incoming = "DiffAdd",
-				current = "DiffText",
-			},
-		},
-		keys = {
-			-- Default mappings
-			-- co — choose ours
-			-- ct — choose theirs
-			-- cb — choose both
-			-- c0 — choose none
-			-- ]x — move to previous conflict
-			-- [x — move to next conflict
-			{ "<leader>C", "<cmd>:GitConflictListQf<cr>", "Show conflicts list" },
-		},
+		config = true,
+    -- default mappings:
+		--  - co choose ours
+		--  - ct choose theirs
+		--  - cb choose both
+		--  - c0 choose none
+		--  - ]x move to previous conflict
+		--  - [x move to next conflict
+    init = function()
+			vim.api.nvim_create_autocmd({ "User" }, {
+				pattern = { "GitConflictDetected" },
+				callback = function(ev)
+					print("Conflict detected in " .. vim.fn.expand("<afile>"))
+					vim.keymap.set("n", "cww", function()
+						engage.conflict_buster()
+						create_buffer_local_mappings()
+					end)
+				end,
+			})
+		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
