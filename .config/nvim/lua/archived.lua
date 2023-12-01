6adf48c1202b7163c7748bb994f721faeb18137c
jean-francois Defez <jean-francois@ayctor.com>
2023-12-01 14:06:50 +0100

    style(scheme): github light scheme

--- .config/nvim/lua/archived.lua
+++ .config/nvim/lua/archived.lua
@@ -1,5 +1,17 @@
 return {
 	{
+		"oxfist/night-owl.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.cmd([[
+	       colorscheme night-owl
+	       highlight DiagnosticUnderlineError gui=undercurl
+	       highlight DiagnosticUnderlineWarn gui=undercurl
+      ]])
+		end,
+	},
+	{
 		"projekt0n/github-nvim-theme",
 		lazy = false,
 		priority = 1000,
--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -4,15 +4,20 @@ return {
 		priority = 100,
 	},
 	{
-		"oxfist/night-owl.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			vim.cmd([[
-	       colorscheme night-owl
-	       highlight DiagnosticUnderlineError gui=undercurl
-	       highlight DiagnosticUnderlineWarn gui=undercurl
-      ]])
+			require("github-theme").setup({
+				options = {
+					styles = {
+						comments = "italic",
+						keywords = "bold",
+						types = "italic,bold",
+					},
+				},
+			})
+			vim.cmd("colorscheme github_light")
 		end,
 	},
 }
