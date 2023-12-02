aa6a23cf2596a56074a6aa66a892978f91d95e9b
jdefez <jdefez@gmail.com>
2023-12-02 12:50:09 +0100

    fix(norg): set defalut workspace to home

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -3,9 +3,9 @@ return {
 		"nvim-neorg/neorg",
 		build = ":Neorg sync-parsers",
 		dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>N", ":Neorg<CR>", desc = "Neorg open" },
-    },
+		keys = {
+			{ "<leader>N", ":Neorg<CR>", desc = "Neorg open" },
+		},
 		config = function()
 			require("neorg").setup({
 				load = {
@@ -13,19 +13,19 @@ return {
 					["core.defaults"] = {},
 					-- Adds pretty icons to your documents
 					["core.concealer"] = {},
-          ["core.keybinds"] = {
-            config = {
-              default_keybinds = true,
-              -- neorg_leader = "<LocalLeader>",
-            },
-          },
+					["core.keybinds"] = {
+						config = {
+							default_keybinds = true,
+							-- neorg_leader = "<LocalLeader>",
+						},
+					},
 					["core.dirman"] = {
 						config = {
 							workspaces = {
 								notes = "~/.config/notes",
-								test = "~/.config/notes/test",
+								home = "~/.config/notes/home",
 							},
-							default_workspace = "notes",
+							default_workspace = "home",
 						},
 					},
 					-- Manages Neorg workspaces
