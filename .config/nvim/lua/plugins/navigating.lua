7cbccdb0df3241a5c523fcc448338f73be40843c
jdefez <jdefez@gmail.com>
2023-12-07 19:26:15 +0100

    feat(plugin): replacing murmur with cursorline

--- .config/nvim/lua/plugins/navigating.lua
+++ .config/nvim/lua/plugins/navigating.lua
@@ -26,23 +26,17 @@ return {
     },
 	},
 	{
-		"nyngwang/murmur.lua",
-		opts = {
-			cursor_rgb = {
-				guibg = nil,
-				guifg = "#B17855",
+		"yamatsum/nvim-cursorline",
+		config = {
+			cursorline = {
+				enable = true,
+				timeout = 500,
+				number = false,
 			},
-			-- cursor_rgb_always_use_config = true,
-			max_len = 80,
-			min_len = 3,
-			exclude_filetypes = {},
-			callbacks = {
-				-- to trigger the close_events of vim.diagnostic.open_float.
-				function()
-					-- Close floating diag. and make it triggerable again.
-					vim.cmd("doautocmd InsertEnter")
-					vim.w.diag_shown = false
-				end,
+			cursorword = {
+				enable = true,
+				min_length = 3,
+				hl = { underline = true },
 			},
 		},
 	},
