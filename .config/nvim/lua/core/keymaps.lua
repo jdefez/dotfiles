c63316a38c9c1a7774e3eee041555ed5a0dc2e33
jdefez <jdefez@gmail.com>
2023-12-09 13:11:41 +0100

    refactor: cleaning dead code

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,2 @@
 vim.keymap.set('n', 'Y', 'yy$')
 vim.keymap.set('i', 'jj', '<ESC>')
-
--- vim.keymap.set('n', '<S-Down>', ':m .+1<CR>==', { silent = true })
--- vim.keymap.set('v', '<S-Down>', ":m '>+1<CR>gv=gv", { silent = true })
--- vim.keymap.set('n', '<S-Up>', ':m .-2<CR>==', { silent = true })
--- vim.keymap.set('v', '<S-Up>', ":m '<-2<CR>gv=gv", { silent = true })
