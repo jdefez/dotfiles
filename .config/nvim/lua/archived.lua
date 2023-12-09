3033c71aa3d91fbde494e199747a99c62fb6c254
jdefez <jdefez@gmail.com>
2023-12-09 13:40:04 +0100

    feat(colorscheme): using rose-pine

--- .config/nvim/lua/archived.lua
+++ .config/nvim/lua/archived.lua
@@ -1,5 +1,22 @@
 return {
 	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("github-theme").setup({
+				options = {
+					styles = {
+						comments = "italic",
+						keywords = "bold",
+						types = "italic,bold",
+					},
+				},
+			})
+			vim.cmd("colorscheme github_dark_dimmed")
+		end,
+	},
+	{
 		"oxfist/night-owl.nvim",
 		lazy = false,
 		priority = 1000,
