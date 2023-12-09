02c77a7f07c98f2cf341f0b3f8d3ba9bb66d5f4b
jdefez <jdefez@gmail.com>
2023-12-09 13:42:03 +0100

    feat(colorscheme): using default background setup

--- .config/nvim/lua/plugins/colorschmes.lua
+++ .config/nvim/lua/plugins/colorschmes.lua
@@ -9,7 +9,7 @@ return {
 		config = function()
 			require("rose-pine").setup({
 				--- @usage 'auto'|'main'|'moon'|'dawn'
-				variant = "moon",
+				variant = "auto",
 				--- @usage 'main'|'moon'|'dawn'
 				dark_variant = "main",
 				bold_vert_split = false,
