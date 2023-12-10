07834e66e71d570fc9f5abee15192756412c2e18
jdefez <jdefez@gmail.com>
2023-12-10 15:23:35 +0100

    style: indentation fixes

--- .config/nvim/lua/plugins/navigating.lua
+++ .config/nvim/lua/plugins/navigating.lua
@@ -18,8 +18,18 @@ return {
 		},
     -- stylua: ignore
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash"
+      },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter"
+      },
     },
 	},
 	{
