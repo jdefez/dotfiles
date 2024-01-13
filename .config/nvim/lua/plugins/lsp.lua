f833ee662be71d9c9fa4d4abf75075cd2a635c52
jean-francois Defez <jean-francois@ayctor.com>
2024-01-13 11:20:23 +0100

    feat(lsp): adding action hint plugin

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -175,4 +175,12 @@ return {
       },
     },
   },
+  {
+    "roobert/action-hints.nvim",
+    config = function()
+      require("action-hints").setup({
+        use_virtual_text = true,
+      })
+    end,
+  },
 }
