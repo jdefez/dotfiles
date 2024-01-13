7d24829b79c21e0c779e787d33ce28b53bf854ff
jean-francois Defez <jean-francois@ayctor.com>
2024-01-13 11:20:47 +0100

    build: locked plugins and style

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -70,8 +70,8 @@ return {
         -- formatting = cmp_format,
         formatting = {
           format = require("lspkind").cmp_format({
-            mode = "symbol", -- show only symbol annotations
-            maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            mode = "symbol",       -- show only symbol annotations
+            maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
             ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
           }),
         },
@@ -209,4 +209,7 @@ return {
     lazy = false,
     branch = "master",
   },
+  {
+    "mateuszwieloch/automkdir.nvim"
+  }
 }
