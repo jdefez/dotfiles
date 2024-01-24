a44f803f511fa92ceca160944fb848e9e72acf4c
jean-francois Defez <jean-francois@ayctor.com>
2024-01-24 18:28:19 +0100

    fix: cursor color

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,13 @@
-require("core.api")
 require("core.dotfiles")
+require("core.api")
 require("core.keymaps")
 require("core.lazy")
 require("core.lsp")
 
--- NOTE: nice to have colorschemes:
--- - https://github.com/nyoom-engineering/nyoom.nvim
--- - https://github.com/mcchrish/vim-no-color-collections
+-- cursor customization
+
+vim.o.guicursor = "n-v-c-sm-i-ci-ve:hor25,r-cr-o:hor25,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
+vim.cmd([[
+  hi Cursor guifg=red guibg=red
+  hi lCursor guifg=red guibg=red
+]])
