e3e589739a01b3b3034f99eb2574227247a1dba6
jdefez <jdefez@gmail.com>
2024-05-19 11:54:50 +0200

    feat: colorscheme update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,7 @@ require("core.lazy")
 require("core.lsp")
 
 vim.cmd([[
+  colorscheme default
   highlight DiagnosticUnderlineError gui=undercurl
   highlight DiagnosticUnderlineWarn gui=undercurl
 ]])
