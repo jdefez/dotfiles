f4a05de4706bcdb67bf54b2a4f84b4f5500716aa
jean-francois Defez <jean-francois@ayctor.com>
2024-10-22 09:16:12 +0200

    feat(nvim): restore underline cursor

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,3 +4,10 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
+
+-- NOTE: Using ufo provider need a large value, feel free to decrease the value
+o.foldlevel = 99
+o.foldlevelstart = 99
+o.foldenable = true
+
+o.guicursor = "n-v-c-sm-i-ci-ve:hor25,r-cr-o:hor25,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
