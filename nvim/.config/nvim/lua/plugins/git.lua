5053a0f74545b6aeb2d1984fc39db464b69e0089
jean-francois Defez <jean-francois@ayctor.com>
2024-10-28 10:21:27 +0100

    feat(nvim): add missing plugin for git conflicts

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -14,7 +14,7 @@ return {
       vim.api.nvim_create_autocmd({ "User" }, {
         pattern = { "GitConflictDetected" },
         callback = function(ev)
-          print("Conflict detected in " .. vim.fn.expand("<afile>"))
+          print("Conflict detected in " .. vim.fn.expand "<afile>")
           vim.keymap.set("n", "cww", function()
             engage.conflict_buster()
             create_buffer_local_mappings()
@@ -32,5 +32,30 @@ return {
     keys = {
       { "<leader>G", "<cmd>Neogit kind=vsplit<CR>", desc = "Neogit open" },
     },
+  },
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = true,
+    -- default mappings
+
+    -- co — choose ours
+    -- ct — choose theirs
+    -- cb — choose both
+    -- c0 — choose none
+    -- ]x — move to previous conflict
+    -- [x — move to next conflict
+    init = function()
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = { "GitConflictDetected" },
+        callback = function(ev)
+          print("Conflict detected in " .. vim.fn.expand "<afile>")
+          vim.keymap.set("n", "cww", function()
+            engage.conflict_buster()
+            create_buffer_local_mappings()
+          end)
+        end,
+      })
+    end,
   },
 }
