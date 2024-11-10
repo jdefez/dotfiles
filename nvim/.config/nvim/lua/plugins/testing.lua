97a62e9a56c5167ea36d90aa337bcd0833ef946a
jdefez <jdefez@gmail.com>
2024-11-10 13:51:01 +0100

    feat(nvim): set up for neotest

---
+++ nvim/.config/nvim/lua/plugins/testing.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    ft = { "php" },
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require "neotest-pest" {
+            ignore_dirs = { "vendor", "node_modules" },
+          },
+        },
+      }
+    end,
+    keys = {
+      {
+        mode = { "n" },
+        "<leader>tn",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run nearest tests",
+      },
+      {
+        mode = { "n" },
+        "<leader>tn",
+        function()
+          require("neotest").run.run(vim.fn.expand "%")
+        end,
+        desc = "Run test file",
+      },
+      {
+        mode = { "n" },
+        "<leader>ts",
+        function()
+          require("neotest").run.run { suite = true }
+        end,
+        desc = "Run test suite",
+      },
+    },
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "V13Axel/neotest-pest",
+    },
+  },
+}
