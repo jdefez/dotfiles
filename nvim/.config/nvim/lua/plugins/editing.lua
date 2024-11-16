6914090f69a28984efc6bdc8c20f441642e26269
jdefez <jdefez@gmail.com>
2024-11-16 12:44:22 +0100

    feat(nvim): install folke/ts-comments

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has "nvim-0.10.0" == 1,
+  },
+  {
     "HiPhish/rainbow-delimiters.nvim",
     lazy = true,
     config = function()
