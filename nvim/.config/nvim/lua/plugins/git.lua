deefc2103931deb2db524e8fc8e5ff64dd7ce516
jean-francois Defez <jean-francois@ayctor.com>
2025-03-13 14:05:25 +0100

    fix(nvim): remove duplicate plugin configuration

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,29 +1,5 @@
 return {
   {
-    "akinsho/git-conflict.nvim",
-    version = "*",
-    config = true,
-    -- default mappings:
-    --  - co choose ours
-    --  - ct choose theirs
-    --  - cb choose both
-    --  - c0 choose none
-    --  - ]x move to previous conflict
-    --  - [x move to next conflict
-    init = function()
-      vim.api.nvim_create_autocmd({ "User" }, {
-        pattern = { "GitConflictDetected" },
-        callback = function(ev)
-          print("Conflict detected in " .. vim.fn.expand "<afile>")
-          vim.keymap.set("n", "cww", function()
-            engage.conflict_buster()
-            create_buffer_local_mappings()
-          end)
-        end,
-      })
-    end,
-  },
-  {
     "NeogitOrg/neogit",
     opts = {},
     dependencies = {
@@ -34,6 +10,9 @@ return {
     },
   },
   {
+    -- TODO:
+    --  - add keybinding for GitConflictListQf
+    --  - handle highlighting of conflicts in chadrc
     "akinsho/git-conflict.nvim",
     version = "*",
     lazy = false,
