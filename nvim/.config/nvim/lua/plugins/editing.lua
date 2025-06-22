864502ef1d758e66b602341a0977737e4d63af95
jean-francois Defez <jean-francois@ayctor.com>
2025-06-22 10:40:45 +0200

    feat(nvim): install surround

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -157,4 +157,14 @@ return {
       hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
