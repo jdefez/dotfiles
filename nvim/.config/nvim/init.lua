0355ed2cbb2eb7095fef98dfd8aa7509f7a6fc97
jdefez <jdefez@gmail.com>
2025-11-15 18:14:54 +0100

    feat(neovim): improve oil

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,6 +24,11 @@ require('rose-pine').setup({
         VirtColumn = { fg = "love" },
         CurSearch = { fg = "base", bg = "leaf", inherit = false },
         Search = { fg = "text", bg = "leaf", blend = 20, inherit = false },
+        OilGitAdded = { fg = "foam" },
+        OilGitModified = { fg = "rose" },
+        OilGitRenamed = { fg = "pine" },
+        OilGitUntracked = { fg = "subtle" },
+        OilGitIgnored = { fg = "muted" }
     },
     groups = {
         border = "muted",
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -60,6 +60,7 @@ vim.pack.add({
     ----------------------------------------------------------------------------
 
     { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/benomahony/oil-git.nvim" }, -- depends on oil
     { src = "https://github.com/echasnovski/mini.pick" },
     { src = "https://github.com/folke/flash.nvim" },
     { src = "https://github.com/stevearc/quicker.nvim" },
