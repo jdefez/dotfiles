bc92c5809ae3ff369399d426405ff111121279b6
jdefez <jdefez@gmail.com>
2025-11-15 17:43:08 +0100

    fix(neovim): git diff colors on hover

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,6 +25,36 @@ require('rose-pine').setup({
         CurSearch = { fg = "base", bg = "leaf", inherit = false },
         Search = { fg = "text", bg = "leaf", blend = 20, inherit = false },
     },
+    groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
+
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
+
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+    },
 })
 
 -- cmd.colorscheme "rose-pine-main"
