fc589fa52f9bfb20370a83896a98fcb1cf69ac7c
jean-francois Defez <jean-francois@ayctor.com>
2025-11-30 17:03:24 +0100

    feat(neovim): switching phpactor plugin

--- nvim/.config/nvim/lua/plugins/phpactor.lua
+++ nvim/.config/nvim/lua/plugins/phpactor.lua
@@ -1,13 +1,36 @@
 return {
   {
-    "phpactor/phpactor",
-    ft = { "php" },
-    event = "VeryLazy",
-    config = function()
-      vim.g.phpactorCompletionIgnoreCase = true
-    end,
+    "gbprod/phpactor.nvim",
+    ft = "php",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+      -- If the update/install notification doesn't show properly,
+      -- you should also add here UI plugins like "folke/noice.nvim" or "stevearc/dressing.nvim"
+    },
     keys = {
-      { "<leader>p", ":PhpactorContextMenu<CR>", desc = "Phpactor context menu" },
+      {
+        "<leader>pm",
+        function()
+          require("phpactor").rpc("context_menu", {})
+        end,
+        desc = "Phpactor context menu",
+      },
+    },
+    opts = {
+      install = {
+        path = vim.fn.stdpath "data" .. "/mason/packages/phpactor/",
+        branch = "master",
+        bin = vim.fn.stdpath "data" .. "/mason/packages/phpactor/phpactor.phar",
+        php_bin = "php",
+        composer_bin = "composer",
+        git_bin = "git",
+        check_on_startup = "none",
+      },
+      lspconfig = {
+        enabled = false,
+        options = {},
+      },
     },
   },
 }
