2f2d4dd41282fb265f2c8620d9003b91d286d1ab
jdefez <jdefez@gmail.com>
2025-12-13 20:27:46 +0100

    feat(fish): aliases setup + plugin: autopair

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,22 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# git aliases
+
+abbr -a -- gst 'git status'
+abbr -a -- gco 'git checkout'
+abbr -a --position anywhere --command git -- co checkout
+abbr -a --position anywhere --command git -- st status
+
+# nvim aliases
+
+abbr -a -- nv nvim
+abbr -a -- vi nvim
+
+# php aliases
+
+abbr -a -- psa ./vendor/bin/phpstan
+abbr -a -- rec ./vendor/bin/rector
+abbr -a -- pst ./vendor/bin/pest
+abbr -a -- pi ./vendor/bin/pint
