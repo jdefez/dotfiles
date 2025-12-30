4aec06aec015df080077c520c46df807fefea5da
jdefez <jdefez@gmail.com>
2025-12-30 15:32:48 +0100

    feat(neovim): pack user commands improved

--- nvim/.config/nvim/lua/commands/pack.lua
+++ nvim/.config/nvim/lua/commands/pack.lua
@@ -1,34 +1,42 @@
-local function complete_packages()
-  return vim.iter(vim.pack.get())
-    :map(function(pack) return pack.spec.name end)
-    :totable()
+local function complete_packages(ArgLead)
+    local packages = vim.iter(vim.pack.get())
+        :map(function(pack) return pack.spec.name end)
+        :totable()
+
+    if ArgLead ~= "" then
+        packages = vim.tbl_filter(function(item)
+            return vim.startswith(item, ArgLead)
+        end, packages)
+    end
+
+    return packages
 end
 
 vim.api.nvim_create_user_command(
-  'PackUpdate',
-  function(info)
-    if #info.fargs ~= 0 then
-      vim.pack.update(info.fargs, { force = info.bang })
-    else
-      vim.pack.update(nil, { force = info.bang })
-    end
-  end,
-  {
-    desc = 'Update packages',
-    nargs = '*',
-    bang = true,
-    complete = complete_packages,
-  }
+    'PackUpdate',
+    function(info)
+        if #info.fargs ~= 0 then
+            vim.pack.update(info.fargs, { force = info.bang })
+        else
+            vim.pack.update(nil, { force = info.bang })
+        end
+    end,
+    {
+        desc = 'Update packages',
+        nargs = '*',
+        bang = true,
+        complete = complete_packages,
+    }
 )
 
 vim.api.nvim_create_user_command(
-  'PackDelete',
-  function(info)
-    vim.pack.del(info.fargs)
-  end,
-  {
-    desc = 'Delete packages',
-    nargs = '+',
-    complete = complete_packages,
-  }
+    'PackDelete',
+    function(info)
+        vim.pack.del(info.fargs)
+    end,
+    {
+        desc = 'Delete packages',
+        nargs = 1,
+        complete = complete_packages,
+    }
 )
