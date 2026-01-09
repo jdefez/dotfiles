d106286e29e958e483602e0833698fac8c6cc4f5
jean-francois Defez <jean-francois@ayctor.com>
2026-01-09 18:45:16 +0100

    feat(fish): adding java 17 path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Java 17 (for SonarLint and other tools)
+fish_add_path /opt/homebrew/opt/openjdk@17/bin
+
 set -xg EDITOR nvim
 set -g CDPATH . ~ ~/.config ~/Code
