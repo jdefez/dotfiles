6aa270c8efbf5920cd98e7f047178442d8199626
jean-francois Defez <jean-francois@ayctor.com>
2026-01-16 18:39:31 +0100

    fix(fish): add missing herd php variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,12 +6,24 @@ end
 fish_add_path /usr/local/bin
 fish_add_path /opt/homebrew/opt/openjdk@17/bin
 fish_add_path /Users/ayctor/.local/bin
+# fish_add_path /opt/homebrew/bin
 fish_add_path "/Users/ayctor/Library/Application Support/Herd/config/nvm"
 fish_add_path "/Users/ayctor/Library/Application Support/Herd/bin"
 fish_add_path /Users/ayctor/.cargo/bin
-fish_add_path /opt/homebrew/bin
 fish_add_path /Users/ayctor/.composer/vendor/bin
 fish_add_path /usr/local/bin/nvim-macos-x86_64/bin
+
+# Herd injected PHP 8.4 configuration.
+export HERD_PHP_84_INI_SCAN_DIR="/Users/ayctor/Library/Application Support/Herd/config/php/84/"
+
+# Herd injected PHP 8.3 configuration.
+export HERD_PHP_83_INI_SCAN_DIR="/Users/ayctor/Library/Application Support/Herd/config/php/83/"
+
+# # Herd injected PHP 8.2 configuration.
+export HERD_PHP_82_INI_SCAN_DIR="/Users/ayctor/Library/Application Support/Herd/config/php/82/"
+
+# Herd injected PHP 8.1 configuration.
+export HERD_PHP_81_INI_SCAN_DIR="/Users/ayctor/Library/Application Support/Herd/config/php/81/"
 
 set -xg EDITOR nvim
 set -g CDPATH . ~ ~/.config ~/Code
